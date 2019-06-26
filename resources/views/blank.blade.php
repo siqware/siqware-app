3b9cac58d5629a62f6c242b28ee4705dc1ff9698
Cham Roeun Tun <chamroeuntun25@outlook.com>
2019-06-26 11:07:25 +0700

    siqware-app

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,83 @@
+@extends('dashboard.app')
+@section('page-title')
+    Blank
+@stop
+@section('page-header')
+    <div class="page-header page-header-light">
+        <div class="page-header-content header-elements-md-inline">
+            <div class="page-title d-flex">
+                <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">Starters</span> - 1 Sidebar</h4>
+                <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
+            </div>
+
+            <div class="header-elements d-none">
+                <a href="#" class="btn btn-labeled btn-labeled-right bg-pink-400">Button <b><i class="icon-menu7"></i></b></a>
+            </div>
+        </div>
+
+        <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+            <div class="d-flex">
+                <div class="breadcrumb">
+                    <a href="index.html" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
+                    <a href="#" class="breadcrumb-item">Link</a>
+                    <span class="breadcrumb-item active">Current</span>
+                </div>
+
+                <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
+            </div>
+
+            <div class="header-elements d-none">
+                <div class="breadcrumb justify-content-center">
+                    <a href="#" class="breadcrumb-elements-item">
+                        Link
+                    </a>
+
+                    <div class="breadcrumb-elements-item dropdown p-0">
+                        <a href="#" class="breadcrumb-elements-item dropdown-toggle" data-toggle="dropdown">
+                            Dropdown
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a href="#" class="dropdown-item">Action</a>
+                            <a href="#" class="dropdown-item">Another action</a>
+                            <a href="#" class="dropdown-item">One more action</a>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item">Separate action</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+@section('page-content')
+    <div class="card">
+        <div class="card-header header-elements-inline">
+            <h5 class="card-title">Basic card</h5>
+            <div class="header-elements">
+                <div class="list-icons">
+                    <a class="list-icons-item" data-action="collapse"></a>
+                    <a class="list-icons-item" data-action="reload"></a>
+                    <a class="list-icons-item" data-action="remove"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <h6 class="font-weight-semibold">Start your development with no hassle!</h6>
+            <p class="mb-3">Common problem of templates is that all code is deeply integrated into the core. This limits your freedom in decreasing amount of code, i.e. it becomes pretty difficult to remove unnecessary code from the project. Limitless allows you to remove unnecessary and extra code easily just by removing the path to specific LESS file with component styling. All plugins and their options are also in separate files. Use only components you actually need!</p>
+
+            <h6 class="font-weight-semibold">What is this?</h6>
+            <p class="mb-3">Starter kit is a set of pages, useful for developers to start development process from scratch. Each layout includes base components only: layout, page kits, color system which is still optional, bootstrap files and bootstrap overrides. No extra CSS/JS files and markup. CSS files are compiled without any plugins or components. Starter kit was moved to a separate folder for better accessibility.</p>
+
+            <h6 class="font-weight-semibold">How does it work?</h6>
+            <p>You open one of the starter pages, add necessary plugins, uncomment paths to files in components.less file, compile new CSS. That's it. I'd also recommend to open one of main pages with functionality you need and copy all paths/JS code from there to your new page, it's just faster and easier.</p>
+        </div>
+    </div>
+@stop
+@push('page-js')
+
+@endpush
+@push('page-css')
+
+@endpush
---
+++ resources/views/dashboard/app.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>SiqWare - @yield('page-title')</title>
+
+    <!-- Global stylesheets -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet"
+          type="text/css">
+    <link href="{{asset('dashboard/material/main_assets/css/icons/icomoon/styles.css')}}" rel="stylesheet"
+          type="text/css">
+    <link href="{{asset('dashboard/material/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('dashboard/material/assets/css/bootstrap_limitless.min.css')}}" rel="stylesheet"
+          type="text/css">
+    <link href="{{asset('dashboard/material/assets/css/layout.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('dashboard/material/assets/css/components.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('dashboard/material/assets/css/colors.min.css')}}" rel="stylesheet" type="text/css">
+    <!-- /global stylesheets -->
+
+    <!-- Custom page style files -->
+    @stack('page-css')
+    <!-- /Custom page style files -->
+
+    <!-- Core JS files -->
+    <script src="{{asset('dashboard/material/main_assets/js/main/jquery.min.js')}}"></script>
+    <script src="{{asset('dashboard/material/main_assets/js/main/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('dashboard/material/main_assets/js/plugins/loaders/blockui.min.js')}}"></script>
+    <script src="{{asset('dashboard/material/main_assets/js/plugins/ui/ripple.min.js')}}"></script>
+    <!-- /core JS files -->
+
+    <!-- Theme JS files -->
+    <script src="{{asset('dashboard/material/assets/js/app.js')}}"></script>
+    <!-- /theme JS files -->
+
+    <!-- route JS files -->
+    @routes
+    <!-- /route JS files -->
+
+    <!-- Custom JS files -->
+    <script src="{{asset('js/custom.js')}}"></script>
+    <!-- /Custom JS files -->
+
+    <!-- Custom page JS files -->
+    @stack('page-js')
+<!-- /Custom page JS files -->
+
+</head>
+
+<body>
+
+<!-- Main navbar -->
+@include('dashboard.navbar')
+<!-- /main navbar -->
+
+
+<!-- Page content -->
+<div class="page-content">
+
+    <!-- Main sidebar -->
+@include('dashboard.sidebar')
+<!-- /main sidebar -->
+
+
+    <!-- Main content -->
+    <div class="content-wrapper">
+
+        <!-- Page header -->
+    @yield('page-header')
+    <!-- /page header -->
+
+
+        <!-- Content area -->
+        <div class="content">
+
+            <!-- Basic card -->
+        @yield('page-content')
+        <!-- /basic card -->
+
+        </div>
+        <!-- /content area -->
+
+
+        <!-- Footer -->
+        <div class="navbar navbar-expand-lg navbar-light">
+            <div class="text-center d-lg-none w-100">
+                <button type="button" class="navbar-toggler dropdown-toggle" data-toggle="collapse"
+                        data-target="#navbar-footer">
+                    <i class="icon-unfold mr-2"></i>
+                    Footer
+                </button>
+            </div>
+
+            <div class="navbar-collapse collapse" id="navbar-footer">
+					<span class="navbar-text">
+						&copy; 2015 - 2018. <a href="#">Limitless Web App Kit</a> by <a
+                                href="http://themeforest.net/user/Kopyov" target="_blank">Eugene Kopyov</a>
+					</span>
+
+                <ul class="navbar-nav ml-lg-auto">
+                    <li class="nav-item">
+                        <a href="#" class="navbar-nav-link">Text link</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="navbar-nav-link">
+                            <i class="icon-lifebuoy"></i>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="https://themeforest.net/item/limitless-responsive-web-application-kit/13080328?ref=kopyov"
+                           class="navbar-nav-link font-weight-semibold">
+								<span class="text-pink-400">
+									<i class="icon-cart2 mr-2"></i>
+									Purchase
+								</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- /footer -->
+
+    </div>
+    <!-- /main content -->
+
+</div>
+<!-- /page content -->
+
+</body>
+</html>
